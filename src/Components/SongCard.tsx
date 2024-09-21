94c856ef36e04136e2a849a2e9cf698ca4439122
Ran <tichraclab26@gmail.com>
2024-09-21 12:43:47 +0300

    convert JSX to javascript

--- src/Components/SongCard.tsx
+++ src/Components/SongCard.tsx
@@ -2,7 +2,7 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import IconButton from "@mui/material/IconButton";
 import Download from "@mui/icons-material/DownloadForOfflineOutlined";
-import CardMedia from "@mui/material/CardMedia";
+import CardMedia, { CardMediaProps } from "@mui/material/CardMedia";
 import Avatar from "@mui/material/Avatar";
 import CardActions from "@mui/material/CardActions";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -12,6 +12,7 @@ import Chip from "@mui/material/Chip";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 
 import { saveAs } from "file-saver";
+import { createElement as E, ImgHTMLAttributes } from "react";
 
 type SongCardProps = {
   imgSrc: string;
@@ -20,63 +21,59 @@ type SongCardProps = {
   tags?: string[];
 };
 
-export default function SongCard(props: SongCardProps) {
-  const { imgSrc, title, creationDate, tags } = props;
+const DEFAULT_AVATAR_IMG =
+  "https://lh3.googleusercontent.com/KAhhnrQQ78MtBWThKhF-3jBjBzAApasY2527JOSSGaCyDQlaJp6FbTCN6enMhnJny97o09kdef9weA=w2880-h1200-p-l90-rj";
 
+export default function SongCard(props: SongCardProps) {
+  const { imgSrc, title, creationDate, tags = [] } = props;
+
+  const cardMediaProps: CardMediaProps & ImgHTMLAttributes<HTMLImageElement> = {
+    component: "img",
+    image: `${imgSrc}?auto=format&fit=crop&w=286`,
+    alt: `Image of ${title}`,
+    height: 194,
+    loading: "lazy",
+  };
+  cardMediaProps;
   function handleDownload() {
     saveAs(imgSrc, title);
   }
 
-  return (
-    <Card sx={{ maxWidth: 345, height: "100%" }}>
-      <CardHeader
-        title={title}
-        subheader={creationDate}
-        avatar={
-          <Avatar
-            src="https://lh3.googleusercontent.com/KAhhnrQQ78MtBWThKhF-3jBjBzAApasY2527JOSSGaCyDQlaJp6FbTCN6enMhnJny97o09kdef9weA=w2880-h1200-p-l90-rj"
-            aria-label="recipe"
-          />
-        }
-        action={
-          <IconButton
-            onClick={handleDownload}
-            aria-label="bookmark Bahamas Islands"
-          >
-            <Download />
-          </IconButton>
-        }
-      />
-      <CardMedia
-        component="img"
-        image={`${imgSrc}?auto=format&fit=crop&w=286`}
-        height={194}
-        loading="lazy"
-      />
-      {tags && (
-        <CardContent>
-          <Grid container spacing={1}>
-            {tags.slice(4).map((tag) => (
-              <Grid>
-                <Chip variant="outlined" label={tag} />
-              </Grid>
-            ))}
-            {tags.length > 4 && (
-              <Grid>
-                <Chip variant="outlined" label="..." />
-              </Grid>
-            )}
-          </Grid>
-        </CardContent>
-      )}
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+  return E(
+    Card,
+    { sx: { maxWidth: 345, height: "100%" } },
+    E(CardHeader, {
+      title: title,
+      subheader: creationDate,
+      avatar: E(Avatar, { src: DEFAULT_AVATAR_IMG, "aria-label": "recipe" }),
+      action: E(
+        IconButton,
+        {
+          onClick: handleDownload,
+          "aria-label": "bookmark Bahamas Islands",
+        },
+        E(Download)
+      ),
+    }),
+    E(CardMedia, cardMediaProps),
+    tags.length > 0 &&
+      E(
+        CardContent,
+        null,
+        E(Grid, { container: true, spacing: 1 }),
+        tags
+          .slice(4)
+          .map((tag) =>
+            E(Grid, null, E(Chip, { variant: "outlined", label: tag }))
+          ),
+        tags.length > 4 &&
+          E(Grid, null, E(Chip, { variant: "outlined", label: "..." }))
+      ),
+    E(
+      CardActions,
+      { disableSpacing: false },
+      E(IconButton, { "aria-label": "add to favorites" }, E(FavoriteIcon)),
+      E(IconButton, { "aria-label": "share" }, E(ShareIcon))
+    )
   );
 }
